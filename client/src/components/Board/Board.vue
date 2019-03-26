98a97a324c9417e21842bbcf065b4bb9af825ff5
Jonathan Wernersson <jonathan2008@live.se>
2019-03-26 23:53:42 +0100

    added basic squares and board component

---
+++ client/src/components/Board/Board.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Board">
+      <Square type="dark" />
+  </div>
+</template>
+
+<script>
+import Square from './Square/Square.vue';
+
+export default {
+  name: 'Board',
+  data() {
+    return {
+        squares: Array(8).fill(0)
+    }
+  },
+  components: {
+      Square
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ client/src/components/Board/Square/Square.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="['Square', type]">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Square',
+  props: {
+    type: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Square {
+  width: 30px;
+  height: 30px;
+}
+  .dark {
+    background-color: black;
+  }
+  .light {
+    background-color: white;
+  }
+</style>
