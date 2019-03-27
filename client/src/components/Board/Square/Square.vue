8eae7f5fa60848e9c1e500a3ad69f79ba724722d
Jonathan Wernersson <jonathan2008@live.se>
2019-03-27 20:36:20 +0100

    edited styling

--- client/src/components/Board/Square/Square.vue
+++ client/src/components/Board/Square/Square.vue
@@ -15,18 +15,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.Square {
-  display: inline-block;
-  margin: 0;
-  padding: 0;
-  border: none;
-  width: 12.5%;
-  height: 12.5%;
-}
+  .Square {
+    display: inline-block;
+    margin: 0;
+    padding: 0;
+    border: none;
+    width: 12.5%;
+    height: 12.5%;
+  }
   .dark {
-    background-color: black;
+    background: linear-gradient(45deg, rgb(70, 52, 3), rgb(59, 47, 8));
   }
   .light {
-    background-color: #ccc;
+    background: linear-gradient(-45deg, rgb(236, 172, 52), rgb(236, 178, 70));
   }
 </style>
